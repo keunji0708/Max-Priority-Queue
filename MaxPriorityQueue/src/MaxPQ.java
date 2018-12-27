f7152b1e194f8a4866705260283a607c8639e383
keunj <keunj@DESKTOP-3FP78GL>
2018-12-27 17:26:58 +0900

    Create Project Max Priority Queue

---
+++ MaxPriorityQueue/src/MaxPQ.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	private int key;
+	private String value;
+
+	public Node() {
+	}
+
+	public Node(int k, String v) {
+		key = k;
+		value = v;
+	}
+
+	public int getkey() {
+		return key;
+	}
+
+	public String getvalue() {
+		return value;
+	}
+
+	public void setkey(int k) {
+		key = k;
+	}
+
+	public void setvalue(String v) {
+		value = v;
+	}
+}
+
+public class MaxPQ {
+
+}
