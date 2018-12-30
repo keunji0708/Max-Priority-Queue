00bca49da254d578d75ea1479113dc298078ae60
keunj <keunj@DESKTOP-3FP78GL>
2018-12-30 19:10:04 +0900

    update main code

--- MaxPriorityQueue/src/MaxPQ.java
+++ MaxPriorityQueue/src/MaxPQ.java
@@ -107,6 +107,88 @@ public class MaxPQ {
 		
 		buildMaxHeap(list);
 		print(list);
+		
+		while(true) {
+			System.out.println("\n------------------------------------");
+			System.out.println("1. 작업 추가          2. 최대값          3.최대 우선순위 작업 처리\n"
+					+ "4. 원소 키값 증가   5. 작업 제거                                 6.종료");
+			System.out.println("------------------------------------\n");
+			Scanner scan = new Scanner(System.in);
+			String input = scan.next();
+			
+			if (input.equals("1")) {
+				System.out.println("<<<<<  작업 추가    >>>>>");
+				System.out.print("우선 순위 입력 : ");
+				int key = scan.nextInt();
+				System.out.print("과목명 입력 : ");
+				String subject = scan.nextLine();
+				subject = ", " + subject;
+				Node newNode = new Node(key, subject);
+				insert(list, newNode);
+				print(list);
+			}
+			else if (input.equals("2")) {
+				System.out.println("<<<<<  최대값    >>>>>");
+				Node max_node = max(list); 
+				System.out.println("우선순위가 최대인 원소 : "+max_node.getkey() + max_node.getvalue());
+				print(list);
+			}
+			else if (input.equals("3")) {
+				System.out.println("<<<<<  최대 우선순위 작업 처리    >>>>>");
+				System.out.print("처리된 최대 우선순위 작업 : ");
+				extract_max(list);
+				print(list);
+				continue;
+			}
+			else if (input.equals("4")) {
+				Node x = new Node();
+				System.out.println("<<<<<  원소 키값 증가    >>>>>");
+				System.out.println("현재 마지막 우선 순위의 번호는 " + (list.size()-1) +"입니다.");
+				while (true) {
+					System.out.print("키 값을 증가시킬 원소의 우선 순위를 입력하세요 : ");
+					scan = new Scanner(System.in);
+					int key = scan.nextInt();
+					
+					if (key >= list.size() || key < 0) {
+						System.out.println("---------- 우선순위가 범위를 벗어났습니다!! ----------");
+						continue;
+						}
+					
+					x = (Node) list.get(key);
+					break;
+					}
+				int key = 0;
+				while (true) {
+					System.out.print("키 값을 얼마로 하시겠나요 [범위 1 ~ 1000] : ");
+					key = scan.nextInt();
+					if (key <= x.getkey()) {
+						System.out.println("---------- 현재 키 값보다 작아지면 안됩니다!! ----------");
+						continue;
+					}
+					else break; 
+				}
+				increase_key(list, x, key); 
+				print(list); 
+			}
+			
+			else if (input.equals("5")) {
+				System.out.println("<<<<<  작업 제거    >>>>>");
+				System.out.print("제거할 작업의 우선 순위를 입력하세요 : ");
+				int count = scan.nextInt(); 
+				Node x = (Node) list.get(count);
+				delete(list, x, count); 
+				print(list);
+				continue;
+			}
+			else if (input.equals("6")) {
+				System.out.println("<<<<<  프로그램을 종료합니다    >>>>>");
+				break;
+			}			
+			else {
+				System.out.println("---------- 다시 입력하세요 ----------");
+				continue;
+			}
+		}
 	}
 	
 	public static void print(List list) {
